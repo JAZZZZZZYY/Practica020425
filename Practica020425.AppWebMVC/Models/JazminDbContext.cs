882b3cf893364c8048e0d64ca2e3cce1a5642e24
Jazmin <ACER@LAPTOP-FRMPB979>
2025-04-02 01:05:21 +1245

    cambios

---
+++ Practica020425.AppWebMVC/Models/JazminDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practica020425.AppWebMVC.Models
+{
+    public class JazminDbContext : DbContext
+    {
+        public JazminDbContext(DbContextOptions<JazminDbContext> options) : base(options) { }
+
+        public DbSet<Proveedor> Proveedores { get; set; }
+
+    }
+}
---
+++ Practica020425.AppWebMVC/Models/Proveedor.cs
@@ -0,0 +1,11 @@
+﻿namespace Practica020425.AppWebMVC.Models
+{
+
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+
+}
